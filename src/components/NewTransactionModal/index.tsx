4fc5e6b470322b985226d0a9cb0b91eb71e1968b
Beatriz Ortiz <bib_ortiz@hotmail.com>
2021-08-10 21:54:28 -0400

    feat: estylized modal inputs and buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/vector.svg'
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -7,28 +8,36 @@ interface NewTransactionModalProps {
 
 }
 
-export function NewTransactionModal({ isOpen, onRequestClone }: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClone: onRequestClose }: NewTransactionModalProps) {
     return (
         <Modal
             isOpen={isOpen}
-            onRequestClose={onRequestClone}
+            onRequestClose={onRequestClose}
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
             <Container>
                 <h2>Cadastrar transação</h2>
 
                 <input
-                placeholder="Título"
+                    placeholder="Título"
                 />
 
                 <input
-                type="number"
-                placeholder="Valor"
+                    type="number"
+                    placeholder="Valor"
                 />
 
                 <input
-                placeholder="Categoria"
+                    placeholder="Categoria"
                 />
 
                 <button type="submit">
